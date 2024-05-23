2ab857dcec33dd8262adca6c1cc3f0bb3155db65
s27502 <s27502@pjwstk.edu.pl>
2024-05-23 18:11:15 +0200

    Modified Program.cs (post working)

--- WebApplication2/WebApplication2/Program.cs
+++ WebApplication2/WebApplication2/Program.cs
@@ -5,8 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+
 builder.Services.AddDbContext<TripContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -14,6 +19,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TripApi v1"));
 }
 
 app.UseHttpsRedirection();
